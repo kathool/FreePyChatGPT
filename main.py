2eec9815928909b79379ca6daef03b70e4214c51
kathool <ntdaggett@gmail.com>
2023-08-07 02:24:49 +0000

    Added code to Replit and added conversation_history variable
    Added required Replit files but main.py is all you need as long as you have the required modules installed, this is just to make development a lot easier.
    
    Added conversation_history which once the script is closed is completely deleted and next time you run the script it won't remember anything, but ChatGPT will remember anything you said previously in the conversation.

--- main.py
+++ main.py
@@ -1,47 +1,71 @@
 print("Importing Modules...\n")
 import requests
+
 print("\n'Requests' imported!")
 import json
+
 print("\n'JSON' imported!")
 import time
+
 print("\n'Time' imported!")
 import colorama as color
+
 color.init()
 print("\n'Colorama' imported!")
 time.sleep(0.5)
-print(color.Fore.GREEN + "\n\nWelcome to Kathool's FreeChatGPT Python Script!\n" + color.Style.RESET_ALL)
+print(color.Fore.GREEN +
+      "\n\nWelcome to Kathool's FreeChatGPT Python Script!\n" +
+      color.Style.RESET_ALL)
+
+conversation_history = []
+
 
 def get_completions(prompt):
-    url = "https://free.churchless.tech/v1/chat/completions" #url = "https://chatgpt-api.shn.hk/v1/" # Alternate API Endpoint
-    headers = {
-        "Content-Type": "application/json",
-    }
-    data = {
-        "messages": [
-            {"role": "system", "content": "You are an AI language model. Please respond with a greeting."},
-            {"role": "user", "content": prompt}
-        ],
-        "max_tokens": 100,
-    }
+  global conversation_history
 
-    response = requests.post(url, headers=headers, json=data)
+  url = "https://free.churchless.tech/v1/chat/completions"  #url = "https://chatgpt-api.shn.hk/v1/" # Alternate API Endpoint
+  headers = {
+    "Content-Type": "application/json",
+  }
+  data = {
+    "messages": conversation_history + [{
+      "role": "user",
+      "content": prompt
+    }],
+    "max_tokens": 100,
+  }
 
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+  response = requests.post(url, headers=headers, json=data)
+
+  if response.status_code == 200:
+    return response.json()
+  else:
+    return None
+
 
 def user_input():
+  global conversation_history
   prompt_text = input(color.Fore.YELLOW + "\nYou: " + color.Style.RESET_ALL)
+  conversation_history.append({
+    "role":
+    "system",
+    "content":
+    "You are an AI language model. Please respond with a greeting."
+  })
+  conversation_history.append({"role": "user", "content": prompt_text})
   response_data = get_completions(prompt_text)
   return response_data
 
-while True:
-    response_data = user_input()
 
-    if response_data:
-        response_str = response_data["choices"][0]["message"]["content"]
-        print(color.Fore.CYAN + "\nAI: " + color.Style.RESET_ALL + response_str)
-    else:
-        print(color.Fore.CYAN + "\nAI: " + color.Fore.RED + "Sorry, there was an error processing your request." + color.Style.RESET_ALL)
-        break
+while True:
+  response_data = user_input()
+
+  if response_data:
+    response_str = response_data["choices"][0]["message"]["content"]
+    conversation_history.append({"role": "assistant", "content": response_str})
+    print(color.Fore.CYAN + "\nAI: " + color.Style.RESET_ALL + response_str)
+  else:
+    print(color.Fore.CYAN + "\nAI: " + color.Fore.RED +
+          "Sorry, there was an error processing your request." +
+          color.Style.RESET_ALL)
+    break
